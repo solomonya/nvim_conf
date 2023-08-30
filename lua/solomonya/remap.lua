f9651fc5269e6a6ea7e78c13418a24f7f5a3cb6c
solomon <hansolomon168@gmail.com>
2023-08-30 11:47:26 +0600

    feat: sync nvim buffers with system clipboard

--- lua/solomonya/remap.lua
+++ lua/solomonya/remap.lua
@@ -1,4 +1,5 @@
 local api = require("nvim-tree.api")
+vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -6,5 +7,4 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<leader>e", api.tree.toggle)
 vim.keymap.set("n", "<leader>o", api.tree.focus)
-vim.keymap.set("i", "jk", "<Esc>")
 
